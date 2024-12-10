de2bb59686a6ca366fa4271e6c54892c228dd2f4
emiiabd <abdalaemiliano@gmail.com>
2024-12-10 19:23:10 -0300

    1.6

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalContextProvider } from './Context/GlobalContext.jsx'
 import './index.css'
